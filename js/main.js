a058cbd6673dee482cb453d9b9957b96b81e3899
unknown <khaleedabuhawwas7@gmail.com>
2020-01-11 20:31:55 +0200

    my

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function () {
+
+    'use starict';
+    //Adjust Slider Heig
+    var winH = $(window).height(),
+
+        upperH = $('.upper-bar').innerHeight(),
+        navH = $('.navbar').innerHeight();
+
+    $('.slider,.carousel-item ').height(winH - (upperH + navH));
+
+    //Featured Work Shuffle
+
+    $('.featured-work ul li').on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active');
+
+
+        if ($(this).data('class') == 'all') {
+            $('.shuffle-images .col-sm').css('opacity', 1);
+        } else {
+            $('.shuffle-images .col-sm').css('opacity', '.09');
+            $($(this).data('class')).parent().css('opacity', 1);
+        }
+
+
+    });
+
+
+})
